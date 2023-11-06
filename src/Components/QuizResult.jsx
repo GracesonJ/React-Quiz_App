741561d0752642288993affd61ffed30ff874125
Graceson j <gracesonjjohnson@gmail.com>
2023-11-06 22:34:08 +0530

    styles updated

--- src/Components/QuizResult.jsx
+++ src/Components/QuizResult.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 function QuizResult(props) {
   return (
 <>
-<>
     <div className='show-score'>
-        Your Score:{props.score}<br/>
-        Total Score:{props.totalScore}
+        Your Score:<br/><br/>
+        <span> {props.score} / {props.totalScore}</span>
     </div>
     <button id="next-button" onClick={props.tryAgain}>Try Again</button>
-    </>
+    
 </> 
  )
 }
